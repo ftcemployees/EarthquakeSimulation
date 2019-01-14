ed6bc4574c310e7d9a3ae7a06ca8a9dc102d7d57
Muller <mando0975@byui.edu>
2019-01-14 16:57:03 -0700

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env #!/bin/sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:ftcemployees/EarthquakeSimulation.git master:gh-pages
